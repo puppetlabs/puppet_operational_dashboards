175fbee3049915adaa2ea9290688a3052bcb6881
Adrian <sensei.loafage@gmail.com>
2022-03-07 12:02:57 -0800

    Lint and add puppet strings documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,46 @@
+# @summary Installs Telegraf, InfluxDB, and Grafana to collect and display Puppet metrics
+# @example Basic usage
+#   include puppet_operational_dashboards
+#
+#   class {'puppet_operational_dashboards':
+#     manage_influxdb => false,
+#     influxdb_host   => '<influxdb_fqdn>',
+#   }
+# @param manage_influxdb
+#   Whether to manage installation and configuration of InfluxDB.  Defaults to true
+# @param manage_grafana
+#   Whether to manage installation and configuration of Grafana.  Defaults to true
+# @param influxdb_host
+#   FQDN of the InfluxDB host.  Defaults to a the value of influxdb::host, or $facts['fqdn'] if unset
+# @param influxdb_port
+#   Port used by the InfluxDB service.  Defaults to the value of influxdb::port, or 8086 if unset
+# @param initial_org
+#   Name of the InfluxDB organization to configure. Defaults to the value of influxdb::initial_org, or 'puppetlabs' if unset
+# @param initial_bucket
+#   Name of the InfluxDB bucket to configure and query. Defaults to the value of influxdb::initial_bucket, or 'puppet_data' if unset
+# @param influxdb_token
+#   InfluxDB admin token in Sensitive format.  Defaults to the value of influxdb::token.
+#   See the puppetlabs/influxdb documentation for more information about this token.
+# @param telegraf_token
+#   Telegraf token in Sensitive format.  This parameter is preferred over $telegraf_token_name if both are given
+# @param telegraf_token_name
+#   Name of the token to retrieve from InfluxDB if not given $telegraf_token.
+# @param manage_telegraf
+#   Whether to manage installation and configuration of Telegraf.  Defaults to true.
+# @param manage_telegraf_token
+#   Whether to create and manage a Telegraf token with permissions to query buckets in the default organization.  Defaults to true.
+# @param use_ssl
+#   Whether to use SSL when querying InfluxDB.  Defaults to true
 class puppet_operational_dashboards (
   Boolean $manage_influxdb = true,
   Boolean $manage_grafana = true,
   String $influxdb_host = lookup(influxdb::host, undef, undef, $facts['fqdn']),
   Integer $influxdb_port = lookup(influxdb::port, undef, undef, 8086),
+  String $initial_org = lookup(influxdb::initial_org, undef, undef, 'puppetlabs'),
+  String $initial_bucket = lookup(influxdb::initial_bucket, undef, undef, 'puppet_data'),
 
   Optional[Sensitive[String]] $influxdb_token = lookup(influxdb::token, undef, undef, undef),
   Optional[Sensitive[String]] $telegraf_token = undef,
-  String $initial_org = lookup(influxdb::initial_org, undef, undef, 'puppetlabs'),
-  String $initial_bucket = lookup(influxdb::initial_bucket, undef, undef, 'puppet_data'),
   # Name of the token to retrive from InfluxDB using the retrieve_token() function if not given $telegraf_token
   String $telegraf_token_name = 'puppet telegraf token',
 
@@ -43,13 +76,13 @@ class puppet_operational_dashboards (
 
       influxdb_org {$initial_org:
         ensure  => present,
-        token       => $influxdb_token,
+        token   => $influxdb_token,
         require => Class['influxdb'],
       }
       influxdb_bucket {$initial_bucket:
         ensure  => present,
         org     => $initial_org,
-        token       => $influxdb_token,
+        token   => $influxdb_token,
         require => [Class['influxdb'], Influxdb_org[$initial_org]],
       }
 
@@ -100,7 +133,7 @@ class puppet_operational_dashboards (
         $telegraf_token
       }
       elsif $influxdb_token {
-        influxdb::retrieve_token($influxdb_uri, $influxdb_token, $telegraf_token_name)
+        Sensitive(influxdb::retrieve_token($influxdb_uri, $influxdb_token, $telegraf_token_name))
       }
       else {
         undef
@@ -108,10 +141,7 @@ class puppet_operational_dashboards (
 
       if $telegraf_token_contents {
         class {'puppet_operational_dashboards::telegraf::agent':
-          token => $telegraf_token_contents ? {
-            Sensitive => $telegraf_token_contents,
-            default   => Sensitive($telegraf_token_contents)
-          }
+          token => $telegraf_token_contents
         }
       }
       else {
@@ -125,10 +155,7 @@ class puppet_operational_dashboards (
 
   if $telegraf_token_contents {
     class {'puppet_operational_dashboards::profile::dashboards':
-      token => $telegraf_token_contents ? {
-        Sensitive => $telegraf_token_contents,
-        default   => Sensitive($telegraf_token_contents)
-      }
+      token => $telegraf_token_contents
     }
   }
 }
--- manifests/telegraf/agent.pp
+++ manifests/telegraf/agent.pp
@@ -1,6 +1,59 @@
+# @summary Installs and configures Telegraf to query hosts in a Puppet infrastructure. Included by the base class
+# @example Basic usage
+#   include puppet_operational_dashboards
+#
+#   class {'puppet_operational_dashboards::telegraf::agent':
+#     token => '<sensitive_telegraf_token>',
+#   }
+# @param token
+#   Telegraf token in Sensitive format.
+# @param influxdb_host
+#   FQDN of the InfluxDB host.  Defaults to $facts['fqdn']
+# @param influxdb_port
+#   Port used by the InfluxDB service.  Defaults to 8086.
+# @param initial_org
+#   Name of the InfluxDB organization. Defaults to 'puppetlabs'.
+# @param initial_bucket
+#   Name of the InfluxDB bucket to query. Defaults to 'puppet_data'.
+# @param use_ssl
+#   Whether to use SSL when querying InfluxDB.  Defaults to true
+# @param manage_ssl
+#   Whether to manage Telegraf ssl configuration.  Defaults to true.
+# @param ssl_cert_file
+#   SSL certificate to be used by the telegraf service.  Defaults to the agent certificate issued by the Puppet CA for the local machine.
+# @param ssl_key_file
+#   Private key used in the CSR for the certificate specified by $ssl_cert_file.
+#   Defaults to the private key of the local machine for generating a CSR for the Puppet CA
+# @param ssl_ca_file
+#   CA certificate issued by the CA which signed the certificate specified by $ssl_cert_file.  Defaults to the Puppet CA.
+# @param version
+#   Version of the Telegraf package to install. Defaults to '1.21.2'.
+# @param collection_method
+#   Determines how metrics will be collected.
+#   'all' will query all Puppet services across all Puppet infrastructure hosts from the node with this class applied.
+#   'local' will query all Puppet services on the node with this class applied.
+#   TODO
+#   'none' will not query any services from the node with this class applied.
+# @param collection_interval
+#   How frequently to collect metrics.  Defaults to '10m'.
+# @param puppetserver_hosts
+#   Array of Puppet server hosts to collect metrics from.  Defaults to all Puppet server hosts in a PE infrastructure.
+#   FOSS users need to supply a list of FQDNs
+# @param puppetdb_hosts
+#   Array of PuppetDB hosts to collect metrics from.  Defaults to all PuppetDB hosts in a PE infrastructure.
+#   FOSS users need to supply a list of FQDNs
+# @param postgres_hosts
+#   Array of Postgres hosts to collect metrics from.  Defaults to all Postgres in a PE infrastructure.
+#   FOSS users need to supply a list of FQDNs.
+# @param profiles
+#   Array of PE profiles on the node with this class applied.  Used when collection_method is set to 'local'.
+#   FOSS users can use the $local_services parameter.
+# @param local_services
+#   Array of FOSS services to collect from when collection_method is set to 'local'.
 class puppet_operational_dashboards::telegraf::agent(
   Sensitive[String] $token,
 
+  #TODO: standardize whether these are lookups or not
   String $influxdb_host = $facts['fqdn'],
   Integer $influxdb_port = 8086,
   String $influxdb_org = 'puppetlabs',
@@ -8,14 +61,12 @@ class puppet_operational_dashboards::telegraf::agent(
 
   Boolean $use_ssl = true,
   Boolean $manage_ssl = true,
-  String  $ssl_cert_file = "${facts['puppet_ssldir']}/certs/${trusted['certname']}.pem",
-  String  $ssl_key_file ="${facts['puppet_ssldir']}/private_keys/${trusted['certname']}.pem",
-  String  $ssl_ca_file ="${facts['puppet_ssldir']}/certs/ca.pem",
+  String  $ssl_cert_file = "/etc/puppetlabs/puppet/ssl/certs/${trusted['certname']}.pem",
+  String  $ssl_key_file ="/etc/puppetlabs/puppet/ssl/private_keys/${trusted['certname']}.pem",
+  String  $ssl_ca_file ='/etc/puppetlabs/puppet/ssl/certs/ca.pem',
 
   String $version = '1.21.2',
   Enum['all', 'local', 'none'] $collection_method = 'all',
-  String  $config_file = '/etc/telegraf/telegraf.conf',
-  String  $config_dir = '/etc/telegraf/telegraf.d',
   String $collection_interval = '10m',
 
   Array $puppetserver_hosts = puppet_operational_dashboards::hosts_with_pe_profile('Master'),
