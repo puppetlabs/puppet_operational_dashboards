96b9f374d98ab9934a996bf5e652d56c31896f9f
Adrian <sensei.loafage@gmail.com>
2022-03-09 10:04:56 -0800

    Fix postgres auth for Telegraf agents
    This commit uses the cert_allowlist_entry defined type to allow access
    to postgres.  It also modifies the function we use to detect profiles to
    allow us to query for these hosts.

---
+++ functions/hosts_with_profile.pp
@@ -0,0 +1,24 @@
+# @summary function used to determine hosts with a profile class
+#
+# Queries PuppetDB for hosts with the specified profile.
+# Used by this module to identify hosts with Puppet Enterprise API endpoints and Telegraf hosts
+#
+# @param profile [String]
+#   The full name of the profile to query.
+#
+# @return [Array[String]]
+#   An array of certnames from the query
+
+function puppet_operational_dashboards::hosts_with_profile(
+  String $profile,
+) >> Array[String] {
+  if $settings::storeconfigs {
+  puppetdb_query("resources[certname] {
+      type = 'Class' and
+      title = '${profile}' and
+      nodes { deactivated is null and expired is null }
+    }").map |$nodes| { $nodes['certname'] }
+  } else {
+    []
+  }
+}
--- manifests/profile/postgres_access.pp
+++ manifests/profile/postgres_access.pp
@@ -4,16 +4,18 @@
 # @param telegraf_hosts
 #   A list of FQDNs running Telegraf to allow access to
 class puppet_operational_dashboards::profile::postgres_access (
-  Array $telegraf_hosts = [$trusted['certname']],
+  Array $telegraf_hosts = puppet_operational_dashboards::hosts_with_profile('Puppet_operational_dashboards::Telegraf::Agent'),
 ) {
+  $ident_file = "/opt/puppetlabs/server/data/postgresql/${facts['pe_postgresql_info']['installed_server_version']}/data/pg_ident.conf"
+
   $telegraf_hosts.each |$host| {
-    $ident_file = "/opt/puppetlabs/server/data/postgresql/${facts['pe_postgresql_info']['installed_server_version']}/data/pg_ident.conf"
-    puppet_enterprise::pg::ident_entry { "telegraf_${host}":
-      pg_ident_conf_path => $ident_file,
-      database           => 'pe-puppetdb',
-      ident_map_key      => 'pe-puppetdb-telegraf-map',
-      client_certname    => $host,
-      user               => 'telegraf',
+    puppet_enterprise::pg::cert_allowlist_entry { "telegraf_${host}":
+      user                          => 'telegraf',
+      database                      => 'pe-puppetdb',
+      allowed_client_certname       => $host,
+      pg_ident_conf_path            => $ident_file,
+      ip_mask_allow_all_users_ssl   => '0.0.0.0/0',
+      ipv6_mask_allow_all_users_ssl => '::/0',
     }
   }
 }
