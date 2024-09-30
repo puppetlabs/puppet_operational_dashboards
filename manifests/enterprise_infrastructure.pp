03b0adf78d6c09a401b1789b5979d2e25be4b285
David Newton <david@davidjnewton.com>
2024-09-30 15:22:27 +0100

    Add yaml templates - remove dependency on toml gem

--- manifests/enterprise_infrastructure.pp
+++ manifests/enterprise_infrastructure.pp
@@ -7,10 +7,13 @@
 #   include puppet_operational_dashboards::enterprise_infrastructure
 # @param profiles
 #   Array of PE profiles on the node with this class applied.
+# @param template_format
+#   Template format to use for puppet template toml or yaml config
 class puppet_operational_dashboards::enterprise_infrastructure (
-  Array[String] $profiles = puppet_operational_dashboards::pe_profiles_on_host(),
+  Array[String] $profiles              = puppet_operational_dashboards::pe_profiles_on_host(),
+  Enum['yaml','toml'] $template_format = 'toml',
 ) {
-  if   ('Puppet_enterprise::Profile::Master' in $profiles) {
+  if   ('Puppet_enterprise::Profile::Master' in $profiles) and ($template_format == 'toml') {
     include influxdb::profile::toml
   }
 
